2931a754ef1b3e005b8e523416df17cf9b8be6a1
kazemek <>
2013-08-27 10:18:38 +0200

    PM-528 fix problems with umlauts

--- ext/modules/payment/paymill/public/javascript/cc.js
+++ ext/modules/payment/paymill/public/javascript/cc.js
@@ -1,38 +1,38 @@
 var isCcSubmitted = false;
-$(document).ready(function () {
-    if (typeof $.fn.prop !== 'function') {
-        $.fn.prop = function(name, value) {
-            if (typeof value === 'undefined') {
-                return this.attr(name);
-            } else {
-                return this.attr(name, value);
-            }
-        };
-    }
+$(document).ready(function() {
+	if (typeof $.fn.prop !== 'function') {
+		$.fn.prop = function(name, value) {
+			if (typeof value === 'undefined') {
+				return this.attr(name);
+			} else {
+				return this.attr(name, value);
+			}
+		};
+	}
 
-    $('#card-owner-field').html('<input type="text" value="' + paymill_cc_holder_val + '" id="paymill-card-owner" class="form-row-paymill" />');
-    $('#card-number-field').html('<input type="text" value="' + paymill_cc_number_val + '" id="paymill-card-number" class="form-row-paymill" />');
-    $('#card-expiry-month-field').html('<select id="paymill-card-expiry-month"></select>');
-    $('#card-expiry-year-field').html('<select id="paymill-card-expiry-year"></select>');
-    $('#card-cvc-field').html('<input type="text" value="' + paymill_cc_cvc_val + '" id="paymill-card-cvc" class="form-row-paymill" size="5" maxlength="4" />');
+	$('#card-owner-field').html('<input type="text" value="' + paymill_cc_holder_val + '" id="paymill-card-owner" class="form-row-paymill" />');
+	$('#card-number-field').html('<input type="text" value="' + paymill_cc_number_val + '" id="paymill-card-number" class="form-row-paymill" />');
+	$('#card-expiry-month-field').html('<select id="paymill-card-expiry-month"></select>');
+	$('#card-expiry-year-field').html('<select id="paymill-card-expiry-year"></select>');
+	$('#card-cvc-field').html('<input type="text" value="' + paymill_cc_cvc_val + '" id="paymill-card-cvc" class="form-row-paymill" size="5" maxlength="4" />');
 
-    for ( var cc_month_counter in paymill_cc_months ) {
-        var cc_month_value = paymill_cc_months[cc_month_counter][0];
-        var cc_month_text = paymill_cc_months[cc_month_counter][1];
+	for (var cc_month_counter in paymill_cc_months) {
+		var cc_month_value = paymill_cc_months[cc_month_counter][0];
+		var cc_month_text = paymill_cc_months[cc_month_counter][1];
 
-        $('<option/>').val(cc_month_value).text(cc_month_text).appendTo($('#paymill-card-expiry-month'));
-    };
+		$('<option/>').val(cc_month_value).text($("<div/>").html(cc_month_text).text()).appendTo($('#paymill-card-expiry-month'));
+	}
+	;
 
-    for ( var cc_year_counter in paymill_cc_years ) {
-        var cc_year_value = paymill_cc_years[cc_year_counter][0];
-        var cc_year_text = paymill_cc_years[cc_year_counter][1];
+	for (var cc_year_counter in paymill_cc_years) {
+		var cc_year_value = paymill_cc_years[cc_year_counter][0];
+		var cc_year_text = paymill_cc_years[cc_year_counter][1];
+		$('<option/>').val(cc_year_value).text(cc_year_text).appendTo($('#paymill-card-expiry-year'));
+	}
 
-        $('<option/>').val(cc_year_value).text(cc_year_text).appendTo($('#paymill-card-expiry-year'));
-    };
-	
 	$('#paymill-card-expiry-month').val(paymill_cc_expiry_month_val);
 	$('#paymill-card-expiry-year').val(paymill_cc_expiry_year_val);
-	
+
 	$('#paymill-card-number').keyup(function() {
 		switch (paymill.cardType($('#paymill-card-number').val()).toLowerCase()) {
 			case 'visa':
@@ -75,14 +75,14 @@ $(document).ready(function () {
 
 		$('.card-icon :first-child').css('position', 'absolute');
 	});
-	
+
 	if (brand !== '') {
 		$('.card-icon').html('<img src="ext/modules/payment/paymill/public/images/32x20_' + brand + '.png" >');
 		$('.card-icon').show();
 		$('.card-icon :first-child').css('position', 'absolute');
 	}
 
-    $('#checkout_confirmation').submit(function () {
+	$('#checkout_confirmation').submit(function() {
 		if (!isCcSubmitted) {
 			if (!paymill_cc_fastcheckout) {
 				if (!paymill.validateExpiry($("#paymill-card-expiry-month option:selected").val(), $("#paymill-card-expiry-year option:selected").val())) {
@@ -99,7 +99,7 @@ $(document).ready(function () {
 					alert(cc_owner_invalid);
 					return false;
 				}
-				
+
 				if (!paymill.validateCvc($("#paymill-card-cvc").val())) {
 					alert(cc_cvc_number_invalid);
 					return false;
@@ -107,55 +107,55 @@ $(document).ready(function () {
 
 				paymill.createToken({
 					number: $("#paymill-card-number").val(),
-					exp_month: $("#paymill-card-expiry-month option:selected").val(), 
-					exp_year: $("#paymill-card-expiry-year option:selected").val(), 
+					exp_month: $("#paymill-card-expiry-month option:selected").val(),
+					exp_year: $("#paymill-card-expiry-year option:selected").val(),
 					cvc: $("#paymill-card-cvc").val(),
 					amount_int: paymill_total,
 					currency: paymill_currency,
 					cardholder: $("#paymill-card-owner").val()
 				}, PaymillCcResponseHandler);
 
-				return false; 
+				return false;
 			} else {
 				$('#paymill_form').html('<input type="hidden" name="paymill_token" value="dummyToken" />').submit();
 			}
 		}
-    });
-	
-	$('#paymill-card-number').focus(function() {
-        paymill_cc_fastcheckout = false;
-        $('#paymill-card-number').val('');
-    });
-    
-    $('#paymill-card-expiry-month').focus(function() {
-        paymill_cc_fastcheckout = false;
-    });
-    
-    $('#paymill-card-expiry-year').focus(function() {
-        paymill_cc_fastcheckout = false;
-    });
-    
-    $('#paymill-card-cvc').focus(function() {
-        paymill_cc_fastcheckout = false;
-        $('#paymill-card-cvc').val('');
-    });
-    
-    $('#paymill-card-owner').focus(function() {
-        paymill_cc_fastcheckout = false;
-        $('#paymill-card-owner').val('');
-    });
+	});
 
-    function PaymillCcResponseHandler(error, result) 
-    { 
+	$('#paymill-card-number').focus(function() {
+		paymill_cc_fastcheckout = false;
+		$('#paymill-card-number').val('');
+	});
+
+	$('#paymill-card-expiry-month').focus(function() {
+		paymill_cc_fastcheckout = false;
+	});
+
+	$('#paymill-card-expiry-year').focus(function() {
+		paymill_cc_fastcheckout = false;
+	});
+
+	$('#paymill-card-cvc').focus(function() {
+		paymill_cc_fastcheckout = false;
+		$('#paymill-card-cvc').val('');
+	});
+
+	$('#paymill-card-owner').focus(function() {
+		paymill_cc_fastcheckout = false;
+		$('#paymill-card-owner').val('');
+	});
+
+	function PaymillCcResponseHandler(error, result)
+	{
 		isCcSubmitted = true;
-        if (error) {
-            isCcSubmitted = false;
+		if (error) {
+			isCcSubmitted = false;
 			alert("An API error occured!");
-            console.log("An API error occured: " + error.apierror);
-            return false;
-        } else {
-            $('#paymill_form').html('<input type="hidden" name="paymill_token" value="' + result.token + '" />');
+			console.log("An API error occured: " + error.apierror);
+			return false;
+		} else {
+			$('#paymill_form').html('<input type="hidden" name="paymill_token" value="' + result.token + '" />');
 			$('#paymill_form').submit();
-        }
-    }
+		}
+	}
 });
